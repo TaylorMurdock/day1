cf02f5f1ccc2006ba6183edb0eacb474292c937f
Taylor Murdock <tjmurd4545@gmail.com>
2023-06-06 22:29:12 -0400

    "Tip Calculator express application created."

--- homework/firsexpressionhomework/server.js
+++ homework/firsexpressionhomework/server.js
@@ -6,6 +6,14 @@ app.get("/greeting/:name", (req, res) => {
   res.send(`Wow! Hello there, ${name}!`);
 });
 
+app.get("/tip/:total/:tipPercentage", (req, res) => {
+  const total = parseFloat(req.params.total);
+  const tipPercentage = parseFloat(req.params.tipPercentage);
+
+  const tipAmount = (total * tipPercentage) / 100;
+  res.send(`your tip will be ${tipAmount.toString()} dollars`);
+});
+
 app.listen(3000, () => {
   console.log("Server listening on port 3000");
 });
